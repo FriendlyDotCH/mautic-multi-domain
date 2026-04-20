d724d3eab8cec5e56ca0a8e8985a54cf168c3816
Matic Zagmajster <maticzagmajster@gmail.com>
2026-04-20 17:57:43 +0200

    Update version

--- Config/config.php
+++ Config/config.php
@@ -4,7 +4,7 @@ return [
     'name'        => 'Multidomain',
     'description' => 'User can add multiple tracking domains for emails.',
     'author'      => 'Abdullah Kiser / Friendly Automate',
-    'version'     => '1.0.1',
+    'version'     => '5.0.0',
     'routes'      => [
         'main' => [
             'mautic_multidomain_index' => [
